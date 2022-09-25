020db090eadd8767a615c9d72e4985616b3b6c2c
pengtang <1033166542@qq.com>
2022-09-25 12:29:53 +0800

    添加seeger

--- deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/quartz/QuartzTask.java
+++ deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/quartz/QuartzTask.java
@@ -26,7 +26,8 @@ public class QuartzTask {
      * @author penTang
      * @date 2022/9/22 14:22
      */
-   @Scheduled(cron = "0/1 * * * * ?")
+   @Scheduled(cron = "0 0 0 * * ?")
+//   @Scheduled(cron = "0/1 * * * * ?")
     public void StartRuleTask() {
         log.info("同步任务开始 =============");
         quarzRoleTaskService.executeQuarzRoleTask();
