354681503200ac008a70436684f6033bfe93bc71
天云天羽 <1013694509@qq.com>
2022-10-12 10:46:20 +0800

    2022-10-12---->EntityMaster 添加新增的字段 windMaster，shenWanMaster ， yyUrban，zhongxinUrban ，ibUrban

--- deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/domain/EntityMaster.java
+++ deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/domain/EntityMaster.java
@@ -10,34 +10,74 @@ import java.util.Date;
 
 /**
  * 【请填写功能名称】对象 entity_master
- * 
+ *
  * @author deloitte
  * @date 2022-09-21
  */
 @Data
 @Accessors(chain = true)
-public class EntityMaster implements Serializable
-{
+public class EntityMaster implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    /** $column.columnComment */
+    /**
+     * $column.columnComment
+     */
     private Long id;
 
-    /** model_master的master_code */
+    /**
+     * model_master的master_code
+     */
     @Excel(name = "model_master的master_code")
     private String masterCode;
 
-    /** entity_info的entity_code */
+    /**
+     * entity_info的entity_code
+     */
     @Excel(name = "entity_info的entity_code")
     private String entityCode;
 
-    /** $column.columnComment */
+    /**
+     * $column.columnComment
+     */
     @Excel(name = "${comment}", readConverterExp = "$column.readConverterExp()")
     private Date created;
 
-    /** $column.columnComment */
+    /**
+     * $column.columnComment
+     */
     @Excel(name = "${comment}", readConverterExp = "$column.readConverterExp()")
     private Date update = new Date();
+
+    /**
+     * entity_info的entity_code
+     */
+    @Excel(name = "wind行业划分")
+    private String windMaster;
+
+    /**
+     * entity_info的entity_code
+     */
+    @Excel(name = "申万行业划分")
+    private String shenWanMaster;
+
+    /**
+     * entity_info的entity_code
+     */
+    @Excel(name = "1、Y：是YY口径下城投机构 2、N：不是YY口径下城投机构")
+    private String yyUrban;
+
+    /**
+     * entity_info的entity_code
+     */
+    @Excel(name = "1、Y：是YY口径下城投机构 2、N：不是YY口径下城投机构")
+    private String zhongxinUrban;
+
+    /**
+     * entity_info的entity_code
+     */
+    @Excel(name = "1、Y：是YY口径下城投机构 2、N：不是YY口径下城投机构")
+    private String ibUrban;
+
 
     @Override
     public String toString() {
