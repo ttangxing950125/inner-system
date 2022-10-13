d77a27314caafee8785a33ae9113da8ed38d4b85
chenjiang <yulin95813@163.com>
2022-10-13 17:15:38 +0800

    可交换转债发行预案

--- deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/strategy/impl/BondConvertibleChangeStrategy.java
+++ deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/strategy/impl/BondConvertibleChangeStrategy.java
@@ -1,10 +1,10 @@
 package com.deloitte.crm.strategy.impl;
 
+import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.StrUtil;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.deloitte.common.core.utils.poi.ExcelUtil;
-import com.deloitte.crm.domain.BondConvertibleChangeInfo;
-import com.deloitte.crm.domain.BondConvertibleInfo;
-import com.deloitte.crm.domain.CrmWindTask;
-import com.deloitte.crm.domain.StockCnInfo;
+import com.deloitte.crm.domain.*;
 import com.deloitte.crm.mapper.*;
 import com.deloitte.crm.service.BondConvertibleChangeInfoService;
 import com.deloitte.crm.service.BondConvertibleInfoService;
@@ -15,6 +15,7 @@ import com.deloitte.crm.strategy.enums.WindTaskEnum;
 import com.deloitte.crm.utils.ApplicationContextHolder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
@@ -22,6 +23,7 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.annotation.Resource;
 import java.util.*;
 import java.util.concurrent.Future;
+import java.util.stream.Collectors;
 
 /**
  * Created with IntelliJ IDEA.
@@ -80,11 +82,62 @@ public class BondConvertibleChangeStrategy implements WindTaskStrategy {
         return null;
     }
 
+    /***
+     * 逻辑餐参看 {@link com.deloitte.crm.strategy.impl.BondConvertibleStrategy}
+     */
     @Async("taskExecutor")
     @Transactional(rollbackFor = Exception.class)
     public Future<Object> doBondImport(BondConvertibleChangeInfo item, Date timeNow, CrmWindTask windTask) {
         item.setTaskId(windTask.getId());
         StockCnInfo stockCnInfo = stockCnInfoService.findByCode(item.getCode());
-        return null;
+        if (stockCnInfo != null) {
+            Long attrId = 0L;
+            EntityStockCnRel entityStockCnRel = entityStockCnRelMapper.selectOne(new LambdaQueryWrapper<EntityStockCnRel>().eq(EntityStockCnRel::getStockDqCode, stockCnInfo.getStockDqCode()).eq(EntityStockCnRel::getStatus, Boolean.TRUE));
+            if (entityStockCnRel != null) {
+                List<EntityAttr> entityAttrsAll = entityAttrMapper.selectList(new LambdaQueryWrapper<EntityAttr>().eq(EntityAttr::getAttrType, 3));
+                EntityAttr filterEntityAttr = entityAttrMapper.selectList(new LambdaQueryWrapper<EntityAttr>().eq(EntityAttr::getAttrType, 3)).stream().filter(e -> ObjectUtil.isNotEmpty(e.getName()) && e.getName().equals("是否有可转债")).findFirst().get();
+                if (filterEntityAttr != null) {
+                    attrId = filterEntityAttr.getId();
+                } else {
+                    EntityAttr entityAttr = new EntityAttr();
+                    EntityAttr entityAttrDB = entityAttrsAll.stream().findFirst().get();
+                    entityAttr.setAttrCateId(entityAttrDB.getAttrCateId());
+                    entityAttr.setAttrCateName(entityAttrDB.getAttrCateName());
+                    entityAttr.setName("是否有可转债");
+                    attrId = Long.valueOf(entityAttrMapper.insertEntityAttr(entityAttr));
+                }
+                EntityInfo entityInfo = entityInfoMapper.selectOne(new LambdaQueryWrapper<EntityInfo>().eq(EntityInfo::getEntityCode, entityStockCnRel.getEntityCode()).eq(EntityInfo::getStatus, 1));
+                if (entityInfo != null) {
+                    EntityAttrValue entityAttrValue = entityAttrValueMapper.selectOne(new LambdaQueryWrapper<EntityAttrValue>().eq(EntityAttrValue::getAttrId, attrId).eq(EntityAttrValue::getEntityCode, entityInfo.getEntityCode()));
+                    Long finalAttrId = attrId;//Y N =>1 0
+                    EntityAttrValue judgeEntityAttrValue = Optional.ofNullable(entityAttrValue).map(e -> e.setValue("1")).orElseGet(() -> EntityAttrValue.builder().value("1").attrId(finalAttrId).entityCode(entityInfo.getEntityCode()).build());
+                    if (judgeEntityAttrValue.getId() != null) {
+                        entityAttrValueMapper.updateById(judgeEntityAttrValue);
+                    } else {
+                        entityAttrValueMapper.insertEntityAttrValue(judgeEntityAttrValue);
+                    }
+                    if (StrUtil.isNotBlank(item.getBondThatName())) {
+                        BondInfo bondInfo = bondInfoMapper.selectOne(new LambdaQueryWrapper<BondInfo>().eq(BondInfo::getBondShortName, item.getBondThatName()));
+                        if (bondInfo != null) {
+                            EntityBondRel judgeEntityBondRel = Optional.ofNullable(entityBondRelMapper
+                                    .selectOne(new LambdaQueryWrapper<EntityBondRel>()
+                                            .eq(EntityBondRel::getBdCode, bondInfo.getBondCode())
+                                            .eq(EntityBondRel::getEntityCode, entityInfo.getEntityCode())))
+                                    .map(e -> e.setConver(Boolean.TRUE)).orElseGet(() -> EntityBondRel.builder()
+                                            .bdCode(bondInfo.getBondCode())
+                                            .conver(Boolean.TRUE)
+                                            .entityCode(entityInfo.getEntityCode())
+                                            .build());
+                            if (judgeEntityBondRel.getId() != null) {
+                                entityBondRelMapper.updateEntityBondRel(judgeEntityBondRel);
+                            } else {
+                                entityBondRelMapper.insertEntityBondRel(judgeEntityBondRel);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return new AsyncResult(new Object());
     }
 }
