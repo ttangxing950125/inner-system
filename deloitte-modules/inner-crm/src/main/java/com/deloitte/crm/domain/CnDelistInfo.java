de47df1c269257fd27b9002f2d7554afc04fb5c6
chenjiang <yulin95813@163.com>
2022-10-13 09:09:45 +0800

    A股退市Excel导入

--- deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/domain/CnDelistInfo.java
+++ deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/domain/CnDelistInfo.java
@@ -2,11 +2,13 @@ package com.deloitte.crm.domain;
 
 import java.util.Date;
 import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import com.deloitte.common.core.annotation.Excel;
 import lombok.Builder;
+
 /**
  * (CnDelistInfo)表实体类
  *
@@ -18,90 +20,112 @@ import lombok.Builder;
 @Data
 @Builder
 public class CnDelistInfo implements Serializable {
+
     private static final long serialVersionUID = -73576670427883067L;
     /**
      * 主键
-     */     @Excel(name = "主键")
+     */
+//    @Excel(name = "主键")
     private Integer id;
     /**
      * 导入时间
-     */     @Excel(name = "导入时间")
+     */
+//    @Excel(name = "导入时间")
     private Date importTime;
     /**
      * 1-新增 2-修改
-     */     @Excel(name = "1-新增 2-修改")
+     */
+//    @Excel(name = "1-新增 2-修改")
     private Integer changeType;
     /**
      * crm_wind_task的id
-     */     @Excel(name = "crm_wind_task的id")
+     */
+//    @Excel(name = "crm_wind_task的id")
     private Integer taskId;
     /**
      * 序号
-     */     @Excel(name = "序号")
+     */
+    @Excel(name = "序号")
     private Integer number;
     /**
      * 代码
-     */     @Excel(name = "代码")
+     */
+    @Excel(name = "代码")
     private String code;
     /**
      * 名称
-     */     @Excel(name = "名称")
+     */
+    @Excel(name = "名称")
     private String name;
     /**
      * 退市日期
-     */     @Excel(name = "退市日期")
+     */
+    @Excel(name = "退市日期")
     private Date delistDate;
     /**
      * 退市时股价(元)
-     */     @Excel(name = "退市时股价(元)")
+     */
+    @Excel(name = "退市时股价(元)")
     private Double delistSharePrice;
     /**
      * 退市时每股净资产(元)
-     */     @Excel(name = "退市时每股净资产(元)")
+     */
+    @Excel(name = "退市时每股净资产(元)")
     private Double delistNetWorth;
     /**
      * 终止上市原因
-     */     @Excel(name = "终止上市原因")
+     */
+    @Excel(name = "终止上市原因")
     private String stopDelistdWhy;
     /**
      * 重组后代码
-     */     @Excel(name = "重组后代码")
+     */
+    @Excel(name = "重组后代码")
     private String reconstitutedCode;
     /**
      * 重组后简称
-     */     @Excel(name = "重组后简称")
+     */
+    @Excel(name = "重组后简称")
     private String reconstitutedName;
     /**
      * 重组后上市日期
-     */     @Excel(name = "重组后上市日期")
+     */
+    @Excel(name = "重组后上市日期")
     private String reconstitutedDelistDate;
     /**
      * 退入三板日期
-     */     @Excel(name = "退入三板日期")
+     */
+    @Excel(name = "退入三板日期")
     private Date threeBoardDate;
     /**
      * 三板代码
-     */     @Excel(name = "三板代码")
+     */
+    @Excel(name = "三板代码")
     private String threeBoardCode;
     /**
      * 三板简称
-     */     @Excel(name = "三板简称")
+     */
+    @Excel(name = "三板简称")
     private String threeBoardName;
     /**
      * 退市股证券类型
-     */     @Excel(name = "退市股证券类型")
+     */
+    @Excel(name = "退市股证券类型")
     private String delistedType;
     /**
      * 重组后证券类型
-     */     @Excel(name = "重组后证券类型")
+     */
+    @Excel(name = "重组后证券类型")
     private String reconstitutedType;
     /**
      * 创建时间
-     */     @Excel(name = "创建时间")
+     */
+//    @Excel(name = "创建时间")
     private Date created;
     /**
      * 更新时间
-     */     @Excel(name = "更新时间")
+     */
+//    @Excel(name = "更新时间")
     private Date updated;
 
 
--- deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/service/CnDelistInfoService.java
+++ deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/service/CnDelistInfoService.java
@@ -1,7 +1,11 @@
 package com.deloitte.crm.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.deloitte.crm.domain.CnCoachBack;
 import com.deloitte.crm.domain.CnDelistInfo;
+import com.deloitte.crm.domain.CrmWindTask;
+
+import java.util.List;
 
 /**
  * (CnDelistInfo)表服务接口
@@ -10,5 +14,11 @@ import com.deloitte.crm.domain.CnDelistInfo;
  * @since 2022-10-11 18:46:11
  */
 public interface CnDelistInfoService extends IService<CnDelistInfo> {
+    /**
+     * @param windTask
+     * @param cnCoachBacks
+     * @return
+     */
+    Object doTask(CrmWindTask windTask, List<CnDelistInfo> cnDelistInfo);
 
 }
