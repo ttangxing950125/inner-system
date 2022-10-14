4c68edf2bd8879bcb8857a0d3a73f02872a65bc9
chenjiang <yulin95813@163.com>
2022-10-14 15:14:35 +0800

    bug fix -主键自动增加

--- deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/domain/EntityGovRel.java
+++ deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/domain/EntityGovRel.java
@@ -1,6 +1,8 @@
 package com.deloitte.crm.domain;
 
 import com.alibaba.fastjson.JSON;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.deloitte.common.core.annotation.Excel;
 import lombok.Data;
 import lombok.experimental.Accessors;
@@ -17,55 +19,79 @@ import java.util.Date;
  */
 @Data
 @Accessors(chain = true)
-public class EntityGovRel implements Serializable
-{
+public class EntityGovRel implements Serializable {
     private static final long serialVersionUID = 1L;
 
-    /** $column.columnComment */
+    /**
+     * $column.columnComment
+     */
+    @TableId(type = IdType.AUTO)
     private Long id;
 
-    /** 城投主体code */
+    /**
+     * 城投主体code
+     */
     @Excel(name = "城投主体code")
     private String entityCode;
 
-    /** 德勤政府code */
+    /**
+     * 德勤政府code
+     */
     @Excel(name = "德勤政府code")
     private String dqGovCode;
 
-    /** 德勤政府code */
+    /**
+     * 德勤政府code
+     */
     @Excel(name = "持股方式")
     private String shareMethod;
 
-    /** 德勤政府code */
+    /**
+     * 德勤政府code
+     */
     @Excel(name = "支持力度")
     private String support;
 
-    /** 德勤政府code */
+    /**
+     * 德勤政府code
+     */
     @Excel(name = "支持力度判断依据")
     private String judgment;
 
-    /** 德勤政府code */
+    /**
+     * 德勤政府code
+     */
     @Excel(name = "持股比例")
     private String shareRatio;
 
-    /** 德勤政府code */
+    /**
+     * 德勤政府code
+     */
     @Excel(name = "持股比例年份")
     private String shareRatioYear;
 
-    /** 德勤政府code */
+    /**
+     * 德勤政府code
+     */
     @Excel(name = "状态 0-禁用 1-启用")
     private String status;
 
-    /** 德勤政府code */
+    /**
+     * 德勤政府code
+     */
     @Excel(name = "备注")
     private String remarks;
 
-    /** 德勤政府code */
+    /**
+     * 德勤政府code
+     */
     @Excel(name = "created")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date created;
 
-    /** 德勤政府code */
+    /**
+     * 德勤政府code
+     */
     @Excel(name = "updated")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date updated;
