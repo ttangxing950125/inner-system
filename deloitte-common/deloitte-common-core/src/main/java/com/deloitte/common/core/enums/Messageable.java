210b8a01aa2bf17d20c6ebafd4b919839fbb9ceb
chenjiang <yulin95813@163.com>
2022-10-20 17:05:37 +0800

    通用自定义枚举接口

---
+++ deloitte-common/deloitte-common-core/src/main/java/com/deloitte/common/core/enums/Messageable.java
@@ -0,0 +1,16 @@
+package com.deloitte.common.core.enums;
+
+import java.io.Serializable;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: chenjiang
+ * @Date: 2022/10/20/17:04
+ * @Description:
+ */
+public interface Messageable extends Serializable {
+    String code();
+
+    String message();
+}
