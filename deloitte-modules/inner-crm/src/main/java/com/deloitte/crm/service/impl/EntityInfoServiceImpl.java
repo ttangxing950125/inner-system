00aa3cdb66d3bc6978894eb37fcf60579c6791cd
天云天羽 <1013694509@qq.com>
2022-10-21 11:20:26 +0800

    2022-10-21---->修改政府主体信息和企业主体信息时，必传id和德勤主体唯一识别码

--- deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/service/impl/EntityInfoServiceImpl.java
+++ deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/service/impl/EntityInfoServiceImpl.java
@@ -2192,10 +2192,8 @@ public class EntityInfoServiceImpl extends ServiceImpl<EntityInfoMapper, EntityI
             }
 
             //修改基础属性，改过曾用名后，需要将曾用名置空
+            entityInfo.setEntityNameHis(null).setEntityNameHisRemarks(null);
             EntityInfo old = entityInfoMapper.selectOne(new QueryWrapper<EntityInfo>().lambda().eq(EntityInfo::getEntityCode, entityInfo.getEntityCode()));
-
-
-
             entityInfoLogsUpdatedService.insert(old.getEntityCode(), old.getEntityName(), old, entityInfo);
             entityInfoMapper.update(entityInfo, new QueryWrapper<EntityInfo>().lambda().eq(EntityInfo::getEntityCode, entityInfo.getEntityCode()));
         }
