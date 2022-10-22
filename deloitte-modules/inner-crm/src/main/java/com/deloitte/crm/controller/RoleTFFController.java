4e0d14063e664148629113c44848005108c02229
天云天羽 <1013694509@qq.com>
2022-10-22 11:32:36 +0800

    2022-10-22---->角色3，4，5任务查询返回优化

--- deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/controller/RoleTFFController.java
+++ deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/controller/RoleTFFController.java
@@ -4,6 +4,7 @@ import com.deloitte.common.core.domain.R;
 import com.deloitte.crm.service.ICrmSupplyTaskService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -32,9 +33,14 @@ public class RoleTFFController {
      * @date 2022/9/27 18:52
      */
     @ApiOperation(value = "获取登录用户角色的信息补充任务")
-    @ApiImplicitParam(name = "taskDate", value = "当天日期 2022-09-29", paramType = "query", dataType = "Integer")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "taskDate", value = "当天日期", example = "2022-09-29",paramType = "query", dataType = "String"),
+            @ApiImplicitParam(name = "pageNum", value = "页码", paramType = "query",example = "1", dataType = "Integer"),
+            @ApiImplicitParam(name = "pageSize", value = "页面大小", paramType = "query", example = "9",dataType = "Integer")
+    })
+
     @PostMapping("/getRoleSupplyTask")
-    public R getRoleSupplyTask(String taskDate) {
-        return iCrmSupplyTaskService.getRoleSupplyTask(taskDate);
+    public R getRoleSupplyTask(String taskDate, Integer pageNum,Integer pageSize) {
+        return iCrmSupplyTaskService.getRoleSupplyTask(taskDate,pageNum,pageSize);
     }
 }
