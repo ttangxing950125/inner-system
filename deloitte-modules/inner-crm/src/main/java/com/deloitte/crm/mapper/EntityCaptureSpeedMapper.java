7f6dce2cc9f3f408a66869d2a0de9f7129836bd1
chenjiang <yulin95813@163.com>
2022-10-27 14:23:31 +0800

    实例类更新补充捕获表Service和Mapper

---
+++ deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/mapper/EntityCaptureSpeedMapper.java
@@ -0,0 +1,16 @@
+package com.deloitte.crm.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.deloitte.crm.domain.EntityCaptureSpeed;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: chenjiang
+ * @Date: 2022/10/27/14:19
+ * @Description:
+ */
+@Mapper
+public interface EntityCaptureSpeedMapper extends BaseMapper<EntityCaptureSpeed> {
+}
---
+++ deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/service/impl/EntityCaptureSpeedServiceImpl.java
@@ -0,0 +1,17 @@
+package com.deloitte.crm.service.impl;
+
+import com.deloitte.crm.service.IEntityCaptureSpeedService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Author: chenjiang
+ * @Date: 2022/10/27/14:21
+ * @Description:
+ */
+@Service
+@Slf4j
+public class EntityCaptureSpeedServiceImpl implements IEntityCaptureSpeedService {
+}
