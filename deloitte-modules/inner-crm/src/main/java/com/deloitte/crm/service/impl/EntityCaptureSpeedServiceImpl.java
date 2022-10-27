723800788e1508a184f3227f729c2dd41c50ed55
天云天羽 <1013694509@qq.com>
2022-10-27 16:53:28 +0800

    20221027---->优化

--- deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/service/impl/EntityCaptureSpeedServiceImpl.java
+++ deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/service/impl/EntityCaptureSpeedServiceImpl.java
@@ -1,28 +1,19 @@
 package com.deloitte.crm.service.impl;
 
-import cn.hutool.core.collection.CollectionUtil;
-import cn.hutool.core.lang.copier.Copier;
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.baomidou.mybatisplus.core.enums.SqlLike;
-import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.deloitte.common.core.domain.R;
 import com.deloitte.common.security.utils.SecurityUtils;
 import com.deloitte.crm.domain.CrmSupplyTask;
-import com.deloitte.common.core.domain.R;
 import com.deloitte.crm.domain.EntityCaptureSpeed;
-import com.deloitte.crm.domain.EntityInfo;
 import com.deloitte.crm.dto.EntityCaptureSpeedDto;
 import com.deloitte.crm.mapper.EntityCaptureSpeedMapper;
 import com.deloitte.crm.service.EntityCaptureSpeedService;
-import org.springframework.beans.factory.annotation.Autowired;
-import com.deloitte.crm.service.IEntityInfoService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
 import java.util.Date;
-
-import javax.annotation.Resource;
 import java.util.List;
 
 /**
@@ -32,8 +23,6 @@ import java.util.List;
 @Slf4j
 @Service
 public class EntityCaptureSpeedServiceImpl extends ServiceImpl<EntityCaptureSpeedMapper, EntityCaptureSpeed> implements EntityCaptureSpeedService {
-    @Resource
-    private IEntityInfoService entityInfoService;
 
     @Autowired
     private EntityCaptureSpeedMapper entityCaptureSpeedMapper;
