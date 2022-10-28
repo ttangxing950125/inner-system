e3d687184ef9b666e995132e5bf1389c15aab774
chenjiang <yulin95813@163.com>
2022-10-28 18:41:47 +0800

    crm_type_info 缓存到redis

--- deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/quartz/CrmTypeInfoQuartz.java
+++ deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/quartz/CrmTypeInfoQuartz.java
@@ -2,6 +2,7 @@ package com.deloitte.crm.quartz;
 
 import com.deloitte.crm.service.CrmTypeInfoService;
 import com.deloitte.crm.service.IEntityAttrService;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
@@ -13,6 +14,7 @@ import javax.annotation.Resource;
  * @Date: 2022/10/28/18:31
  * @Description: crm_type_info 数据缓存
  */
+@Component
 public class CrmTypeInfoQuartz {
 
     @Resource
