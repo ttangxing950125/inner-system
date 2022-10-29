72b31268901b21d06c97c8dac2f1c286d729320f
xiaokekeke213 <819094099@qq.com>
2022-10-29 12:46:30 +0800

    2022.10.28 提交  => 角色7    ==> 忽略接口更改

--- deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/service/impl/RoleSevenIgnore.java
+++ deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/service/impl/RoleSevenIgnore.java
@@ -5,14 +5,16 @@ import com.deloitte.common.core.domain.R;
 import com.deloitte.common.core.exception.ServiceException;
 import com.deloitte.common.security.utils.SecurityUtils;
 import com.deloitte.crm.constants.BadInfo;
-import com.deloitte.crm.domain.CrmEntityTask;
+import com.deloitte.crm.domain.EntityInfo;
 import com.deloitte.crm.dto.EntityInfoInsertDTO;
 import com.deloitte.crm.mapper.CrmEntityTaskMapper;
+import com.deloitte.crm.mapper.EntityInfoMapper;
 import com.deloitte.crm.service.EntityInfoManager;
 import com.deloitte.crm.service.ICrmEntityTaskService;
 import com.deloitte.crm.service.RoleSevenTask;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 
 import java.util.Optional;
 
@@ -27,19 +29,24 @@ public class RoleSevenIgnore implements RoleSevenTask {
 
     private CrmEntityTaskMapper crmEntityTaskMapper;
 
+    private EntityInfoMapper entityInfoMapper;
+
     private EntityInfoManager entityInfoManager;
 
     private ICrmEntityTaskService iCrmEntityTaskService;
 
     @Override
     public R finishTask(EntityInfoInsertDTO entityInfoInsertDTO) {
-        Integer taskId = entityInfoInsertDTO.getTaskId();
-        //查询当条数据
-        CrmEntityTask crmEntityTask = Optional.ofNullable(crmEntityTaskMapper.selectOne(new QueryWrapper<CrmEntityTask>().lambda().eq(CrmEntityTask::getId,taskId))).orElseThrow(() -> new ServiceException(BadInfo.EMPTY_TASK_TABLE.getInfo()));
+        EntityInfo entityInfo;
+        if(ObjectUtils.isEmpty(entityInfoInsertDTO.getCreditCode())){
+            entityInfo = Optional.ofNullable(entityInfoMapper.selectOne(new QueryWrapper<EntityInfo>().lambda().eq(EntityInfo::getEntityName, entityInfoInsertDTO.getEntityName()))).orElseThrow(() -> new ServiceException(BadInfo.EMPTY_ENTITY_INFO.getInfo()));
+        }else{
+            entityInfo = Optional.ofNullable(entityInfoMapper.selectOne(new QueryWrapper<EntityInfo>().lambda().eq(EntityInfo::getCreditCode, entityInfoInsertDTO.getCreditCode()))).orElseThrow(() -> new ServiceException(BadInfo.EMPTY_ENTITY_INFO.getInfo()));
+        }
+        entityInfoInsertDTO.setEntityCode(entityInfo.getEntityCode());
         //绑定关联关系
-//        entityInfoInsertDTO.set
         entityInfoManager.bindData(entityInfoInsertDTO,null, SecurityUtils.getUsername());
         //完成任务
-        return iCrmEntityTaskService.finishTask(taskId,1,null);
+        return iCrmEntityTaskService.finishTask(entityInfoInsertDTO.getTaskId(),1,null);
     }
 }
