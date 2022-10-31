bfc4954434b131910a3bcf93fb3de1d7abb08f73
xiaokekeke213 <819094099@qq.com>
2022-10-31 09:54:55 +0800

    2022.10.31 提交  => 修改 角色7    ==>角色7 bug修改

---
+++ deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/mapper/StockThkImportMapper.java
@@ -0,0 +1,14 @@
+package com.deloitte.crm.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.deloitte.crm.domain.StockThkImport;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author 正杰
+ * @description: StockThkImportMapper
+ * @date 2022/10/30
+ */
+@Mapper
+public interface StockThkImportMapper extends BaseMapper<StockThkImport> {
+}
