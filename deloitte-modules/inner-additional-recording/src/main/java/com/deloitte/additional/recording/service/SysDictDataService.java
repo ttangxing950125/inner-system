37022249b9f0547c40757d765af9ef9e4dfcf18b
天云天羽 <1013694509@qq.com>
2022-11-07 18:09:31 +0800

    20221107---->版本配置页面查询

--- deloitte-modules/inner-additional-recording/src/main/java/com/deloitte/additional/recording/service/SysDictDataService.java
+++ deloitte-modules/inner-additional-recording/src/main/java/com/deloitte/additional/recording/service/SysDictDataService.java
@@ -1,6 +1,7 @@
 package com.deloitte.additional.recording.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.deloitte.common.core.domain.R;
 import com.deloitte.system.api.domain.SysDictData;
 
 /**
@@ -10,5 +11,28 @@ import com.deloitte.system.api.domain.SysDictData;
  * @date 2022/11/7 17:17
 */
 public interface SysDictDataService extends IService<SysDictData> {
-
+    /**
+     * 查询可查询年份
+     *
+     * @return R
+     * @author 冉浩岑
+     * @date 2022/11/7 17:28
+     */
+    R getYear();
+    /**
+     * 查询可查询数据源
+     *
+     * @return R
+     * @author 冉浩岑
+     * @date 2022/11/7 17:28
+     */
+    R getDataSource();
+    /**
+     * 查询可显示类型
+     *
+     * @return R
+     * @author 冉浩岑
+     * @date 2022/11/7 17:28
+     */
+    R getShowType();
 }
--- deloitte-modules/inner-additional-recording/src/main/java/com/deloitte/additional/recording/service/impl/SysDictDataServiceImpl.java
+++ deloitte-modules/inner-additional-recording/src/main/java/com/deloitte/additional/recording/service/impl/SysDictDataServiceImpl.java
@@ -1,10 +1,15 @@
 package com.deloitte.additional.recording.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.deloitte.additional.recording.constants.Common;
 import com.deloitte.additional.recording.mapper.SysDictDataMapper;
 import com.deloitte.additional.recording.service.SysDictDataService;
+import com.deloitte.common.core.domain.R;
 import com.deloitte.system.api.domain.SysDictData;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
 
 /**
  * 字典表服务层
@@ -15,4 +20,40 @@ import org.springframework.stereotype.Service;
 @Service("sysDictDataService")
 public class SysDictDataServiceImpl extends ServiceImpl<SysDictDataMapper, SysDictData> implements SysDictDataService {
 
+    @Resource
+    private SysDictDataMapper sysDictDataMapper;
+
+    /**
+     * 查询可查询年份
+     *
+     * @return R
+     * @author 冉浩岑
+     * @date 2022/11/7 17:28
+     */
+    @Override
+    public R getYear() {
+        return R.ok(sysDictDataMapper.selectList(new QueryWrapper<SysDictData>().lambda().eq(SysDictData::getDictType, Common.SEARCH_YEAR)));
+    }
+    /**
+     * 查询可查询数据源
+     *
+     * @return R
+     * @author 冉浩岑
+     * @date 2022/11/7 17:28
+     */
+    @Override
+    public R getDataSource() {
+        return R.ok(sysDictDataMapper.selectList(new QueryWrapper<SysDictData>().lambda().eq(SysDictData::getDictType, Common.DATA_SOURCE)));
+    }
+    /**
+     * 查询可显示类型
+     *
+     * @return R
+     * @author 冉浩岑
+     * @date 2022/11/7 17:28
+     */
+    @Override
+    public R getShowType() {
+        return R.ok(sysDictDataMapper.selectList(new QueryWrapper<SysDictData>().lambda().eq(SysDictData::getDictType, Common.SHOW_TYPE)));
+    }
 }
