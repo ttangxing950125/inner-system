49f6a26ef140c81368c35e9f2a81f242c2823733
pengtang <1033166542@qq.com>
2022-11-08 10:47:56 +0800

    修改查重

--- deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/service/impl/EntityCaptureSpeedServiceImpl.java
+++ deloitte-modules/inner-crm/src/main/java/com/deloitte/crm/service/impl/EntityCaptureSpeedServiceImpl.java
@@ -17,10 +17,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
 
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 /**
